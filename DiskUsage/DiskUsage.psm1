0e42d7cad72ba0960b741a66db19e167c7630fbd
David Ramos <MrDRamos@Hotmail.com>
2021-07-31 13:36:49 -0400

    Wip DiskUsage

--- DiskUsage/DiskUsage.psm1
+++ DiskUsage/DiskUsage.psm1
@@ -1,11 +1,54 @@
 
 <#
-    Measures the amound of disk space used, and the number of file contianed, by the items in the specified path,
-    The arguments and aliases were patterned after the unix du (disk-usage) utility
+.SYNOPSIS
+Measures the amount of disk space used, and the number of file contained, by the items in the specified path,
+The arguments and aliases were patterned after the UNIX du (disk-usage) utility
+
+.DESCRIPTION
+Long description
+
+.PARAMETER Path
+Parameter description
+
+.PARAMETER Include
+Parameter description
+
+.PARAMETER Exclude
+Parameter description
+
+.PARAMETER FullName
+Parameter description
+
+.PARAMETER Total
+Parameter description
+
+.PARAMETER Summarize
+Parameter description
+
+.PARAMETER All
+Parameter description
+
+.PARAMETER Depth
+Parameter description
+
+.EXAMPLE
+Get usage stats for all files in and under the users document folder
+Measure-DiskUsage $Home\Documents -All
+
+.EXAMPLE
+Get usage stats for .exe files in the 'Program Files' folder. 
+Ignore directories with privileged access errors
+Measure-DiskUsage $ENV:ProgramFiles -ErrorAction Ignore -All -Include *.exe
+
+.EXAMPLE
+Get a single summery for all the files in the 'Program Files' folder. 
+Ignore directories with privileged access errors
+Measure-DiskUsage $ENV:ProgramFiles -ErrorAction Ignore -All -Summarize
 #>
-function Measure-FolderUsage
+function Measure-DiskUsage
 {
     [CmdletBinding()]
+    [OutputType([array])]
     param (
         [Parameter(ValueFromPipeline)]
         [object] $Path = ".",
@@ -36,27 +79,22 @@ function Measure-FolderUsage
         [int] $Depth = 1000
     )
 
-    $ErrorAction = $PSBoundParameters["ErrorAction"]
-    if(-not $errorAction)
-    {
-        $ErrorAction = $ErrorActionPreference
-    }
     [double] $TotalBytesUsed = 0
     [int] $TotalFileCount = 0
     $Depth = [math]::Max($Depth-1, 0)
 
-    if ($All -and $Include -or $Exclude)
+    if ($All -and ($Include -or $Exclude))
     {
         [double] $BytesUsed = 0
         [int] $FileCount = 0
-        $FileS = Get-ChildItem -Path "$Path\*" -File -Include $Include -Exclude $Exclude -ErrorAction $ErrorAction
+        $FileS = Get-ChildItem -Path "$Path\*" -File -Include $Include -Exclude $Exclude -ErrorAction $ErrorActionPreference
         foreach ($Item in $FileS) 
         {
             $BytesUsed += $Item.Length
             $FileCount++ 
             if (!$Summarize)
             {
-                $Name = (if ($FullName) {$Item.FullName} else {$Item.Name})
+                $Name = if ($FullName) {$Item.FullName} else {$Item.Name}
                 $Usage = [PSCustomObject]@{
                     Size = $Item.Length
                     Files = 1
@@ -67,18 +105,18 @@ function Measure-FolderUsage
         }
         $TotalBytesUsed += $BytesUsed
         $TotalFileCount += $FileCount
-        $ItemS = Get-ChildItem -Path $Path -Directory
+        $ItemS = Get-ChildItem -Path $Path -Directory -ErrorAction $ErrorActionPreference
 
     }
     else 
     {
         if ($All)
         {
-            $ItemS = Get-ChildItem -Path $Path -ErrorAction $ErrorAction
+            $ItemS = Get-ChildItem -Path $Path -ErrorAction $ErrorActionPreference
         }
         else 
         {
-            $ItemS = Get-ChildItem -Path $Path -Directory -ErrorAction $ErrorAction
+            $ItemS = Get-ChildItem -Path $Path -Directory -ErrorAction $ErrorActionPreference
         }        
     }
 
@@ -89,7 +127,7 @@ function Measure-FolderUsage
         [int] $FileCount = 0
         if ($Item.PSIsContainer)
         {
-            $FileS = Get-ChildItem -Path $Item.FullName -File -Include $Include -Exclude $Exclude -Recurse -Depth $Depth -ErrorAction $ErrorAction
+            $FileS = Get-ChildItem -Path $Item.FullName -File -Include $Include -Exclude $Exclude -Recurse -Depth $Depth -ErrorAction $ErrorActionPreference
             foreach ($File in $FileS) 
             {
                 $BytesUsed += $File.Length
@@ -116,16 +154,19 @@ function Measure-FolderUsage
         $TotalBytesUsed += $BytesUsed
         $TotalFileCount += $FileCount
     }
+
+    $Name = if ($FullName) {Resolve-Path -Path $Path} else {"$(Split-Path -Path $Path -Leaf)"}
+    $Usage = [PSCustomObject]@{
+        Size = $TotalBytesUsed
+        Files = $TotalFileCount
+        Name = $Name
+    }    
+    Write-Output $Usage
     
-    if ($Total -or $Summarize)
+    if (!$Summarize -and $Total)
     {
-        #$Name = if ($FullName) {Resolve-Path -Path $Path} else {"$(Split-Path -Path $Path -Leaf)"}
-        $Name = "Total"
-        $Usage = [PSCustomObject]@{
-            Size = $TotalBytesUsed
-            Files = $TotalFileCount
-            Name = $Name
-        }    
+        $Usage = $Usage.PSObject.Copy()
+        $Usage.Name = "Total"
         Write-Output $Usage
     }
 }
@@ -133,10 +174,60 @@ function Measure-FolderUsage
 
 
 <#
-    Measures the amound of disk space used, and the number of file contianed, by the items in the specified path,
-    Simular to Measure-FolderUsage. But the uses byte size can be format in KB, MG or GB 
+.SYNOPSIS
+Measures the amount of disk space used, and the number of file contained, by the items in the specified path,
+Similar to Measure-DiskUsage. But the used byte size can be format in Bytes, KB, MB or GB 
+
+.DESCRIPTION
+Long description
+
+.PARAMETER Path
+Parameter description
+
+.PARAMETER Include
+Parameter description
+
+.PARAMETER Exclude
+Parameter description
+
+.PARAMETER FullName
+Parameter description
+
+.PARAMETER Total
+Parameter description
+
+.PARAMETER Summarize
+Parameter description
+
+.PARAMETER All
+Parameter description
+
+.PARAMETER Depth
+Parameter description
+
+.PARAMETER Kilobyte
+Parameter description
+
+.PARAMETER Megabytes
+Parameter description
+
+.PARAMETER Gigabytes
+Parameter description
+
+.PARAMETER Descending
+Parameter description
+
+.EXAMPLE
+Show disk usage for all files in and under the users document folder
+Sort by the folders with the largest number of files
+Show-DiskUsage $Home\Documents -All
+
+.EXAMPLE
+Show disk usage for the ProgramFiles(x86) folder in Megabytes.
+Ignore directories with privileged access errors
+Show-DiskUsage ${ENV:ProgramFiles(x86)} -ErrorAction Ignore -Megabytes
 #>
-function Show-FolderUsage
+function Show-DiskUsage
 {
     [CmdletBinding()]
     param (
@@ -153,7 +244,7 @@ function Show-FolderUsage
         [switch] $FullName,
 
         [Parameter()]
-        [Alias("-c")]
+        [Alias("c")]
         [switch] $Total,
 
         [Parameter()]
@@ -169,15 +260,50 @@ function Show-FolderUsage
         [int] $Depth = 1000,
 
         [Parameter()]
-        [ValidateSet("Bytes", "KB", "MB", "GB")]
-        [string] $Unit = "Bytes",
+        [Alias("kb")]
+        [switch] $Kilobyte,
+
+        [Parameter()]
+        [Alias("mb")]
+        [switch] $Megabytes,
+
+        [Parameter()]
+        [Alias("gb")]
+        [switch] $Gigabytes,
 
         [Parameter()]
         [switch] $Descending
     )
+   
+    if ($Gigabytes) 
+    { 
+        #        123456789-12345 
+        $UHdr = "       Size(GB)"
+        $UFmt = "{0,15:N3}"
+        $Ufac = 1GB
+    }
+    elseif ($Megabytes) 
+    { 
+        #        123456789-12345 
+        $UHdr = "       Size(MB)"
+        $UFmt = "{0,15:N3}"
+        $Ufac = 1MB
+    }
+    elseif ($Kilobyte) 
+    { 
+        #        123456789-12345 
+        $UHdr = "       Size(KB)"
+        $UFmt = "{0,15:N3}"
+        $Ufac = 1KB
+    }
+    else 
+    {
+        #        123456789-12345 
+        $UHdr = "    Size(Bytes)"
+        $UFmt = "{0,15:N0}"
+        $Ufac = 1
+    }
 
-    $Norm = @{Bytes=1; KB=1KB; MB=1MB; GB=1GB}[$Unit]
-    $Usage = Measure-FolderUsage -Path $Path -Include $Include -Exclude $Exclude -FullName:$FullName -Total:$Total -Summarize:$Summarize -Depth $Depth -All:$All
-    $Usage | Sort-Object -Descending:$Descending -Property Size | Format-Table -Property @{Name="Size($Unit)"; Expression={"{0:N3}" -f ($_.Size / $Norm)} }, Files, Name
+    $Usage = Measure-DiskUsage -Path $Path -Include $Include -Exclude $Exclude -FullName:$FullName -Total:$Total -Summarize:$Summarize -Depth $Depth -All:$All
+    $Usage | Sort-Object -Descending:$Descending -Property Size | Format-Table -Property @{Name=$UHdr; Expression={$Ufmt -f ($_.Size / $Ufac)} }, Files, Name
 }
-
