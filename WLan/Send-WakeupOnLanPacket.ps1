a3f9e3877a15cf8bfd7803a712abae8a398a4b43
David Ramos <MrDRamos@Hotmail.com>
2022-01-22 17:16:52 -0500

    Wip Send-WakeupOnLanPacket

--- WLan/Send-WakeupOnLanPacket.ps1
+++ WLan/Send-WakeupOnLanPacket.ps1
@@ -71,47 +71,48 @@ param(
 )
  
 
-function Send-WakeupOnLanPacket([string]$MacAddress)
+function Send-WakeupOnLanPacket([string]$MacAddress, [int] $Port = 9)
 {
     <#
     .SYNOPSIS
     Send-Packet sends a specified number of magic packets to a MAC address in order to wake up the machine.  
  
     .PARAMETER MacAddress
-    A single or array of MAC address's of the machines to wake up.
+    The MAC address of the machine to wake up.
+
+    .PARAMETER Port
+    The UDP datagram to port to use. Default - 9
+        0 = Rreserved port number
+        7 = Echo Protocol
+        9 = Discard Protocol
 
     .NOTES
     Wiki - Wake-on-LAN
     https://en.wikipedia.org/wiki/Wake-on-LAN
-
-    UdpClient.Send Method
-    https://docs.microsoft.com/en-us/dotnet/api/system.net.sockets.udpclient.send?view=net-6.0
-
-    .NOTES
-    Inspired by: Ammaar Limbada, https://gist.github.com/alimbada/4949168
     #>
  
     try
     {
-        $Broadcast = ([System.Net.IPAddress]::Broadcast)
- 
-        ## Create UDP client instance
-        $UdpClient = New-Object Net.Sockets.UdpClient
- 
-        ## Create IP endpoints for each port
-        $IPEndPoint = New-Object Net.IPEndPoint $Broadcast, 9
- 
-        ## Construct physical address instance for the MAC address of the machine (string to byte array)
+        ## Construct .Net address object for the MAC address
         $MacAddress = $MacAddress.Replace(":", "-")
         $MacAddress = $MacAddress.Replace(".", "-")
         $MAC = [Net.NetworkInformation.PhysicalAddress]::Parse($MacAddress.ToUpper())
  
-        ## Construct the Magic Packet frame
+        ## Construct the Magic Packet frame, 17 * 6 = 102 Bytes
         $Packet = [Byte[]](, 0xFF * 6) + ($MAC.GetAddressBytes() * 16)
  
+        ## Create IP endpoints for each port
+        $Broadcast = ([System.Net.IPAddress]::Broadcast)
+        $IPEndPoint = New-Object Net.IPEndPoint $Broadcast, $Port
+ 
         ## Broadcast UDP packets to the IP endpoint of the machine
-        $UdpClient.Send($Packet, $Packet.Length, $IPEndPoint) | Out-Null
+        $UdpClient = New-Object Net.Sockets.UdpClient
+        $SentLength = $UdpClient.Send($Packet, $Packet.Length, $IPEndPoint)
         $UdpClient.Close()
+        if ($SentLength -ne $Packet.Length)
+        {
+            Throw "Failed to broadcast the WOL packet"
+        }
     }
     catch
     {
