cd3ffa93e5abf301ecd38ddb0dfceca1509fa120
David Ramos <MrDRamos@Hotmail.com>
2022-03-24 03:47:21 -0400

    Better documentation of Test-Interlocked.Api.ps1

--- Threading/Test-Interlocked.Api.ps1
+++ Threading/Test-Interlocked.Api.ps1
@@ -1,18 +1,18 @@
-"`nInterlocked.Increment()"
+"`nInterlocked.Increment(`$RetValue = `$NewValue = `$OrgValue + 1)"
 $OrgValue = 0
 $NewValue = $OrgValue
-$RefInput = [ref]$NewValue;
-$RetValue = [Threading.Interlocked]::Increment($RefInput)
+$RefValue = [ref]$NewValue;
+$RetValue = [Threading.Interlocked]::Increment($RefValue)
 ([PSCustomObject]@{ OrgValue = $OrgValue; RetValue = $RetValue; NewValue = $NewValue } | Format-Table | Out-String).Trim()
 
-"`nInterlocked.Add()"
+"`nInterlocked.Add(`$RetValue = `$NewValue = `$OrgValue + `$Increment)"
 $OrgValue = 1
 $NewValue = $OrgValue
 $Increment = 1
 $RetValue = [Threading.Interlocked]::Add([ref]$NewValue, $Increment)
-([PSCustomObject]@{ OrgValue = $OrgValue; RetValue = $RetValue; NewValue = $NewValue } | Format-Table | Out-String).Trim()
+([PSCustomObject]@{ OrgValue = $OrgValue; RetValue = $RetValue; NewValue = $NewValue; Increment = $Increment } | Format-Table | Out-String).Trim()
 
-"`nInterlocked.CompareExchange(Same)"
+"`nInterlocked.CompareExchange(if (`$OrgValue -EQ `$Comparand) then {`$NewValue = 3} )"
 $OrgValue = 0
 $NewValue = $OrgValue
 $RetValueIfTrue = 3
@@ -20,7 +20,7 @@ $Comparand = 0
 $RetValue = [Threading.Interlocked]::CompareExchange([ref]$NewValue, $RetValueIfTrue, $Comparand)
 ([PSCustomObject]@{ OrgValue = $OrgValue; RetValue = $RetValue; NewValue = $NewValue; Comparand = $Comparand } | Format-Table | Out-String).Trim()
 
-"`nInterlocked.CompareExchange(Diff)"
+"`nInterlocked.CompareExchange(if (`$OrgValue -EQ `$Comparand) then {`$NewValue = 4} )"
 $OrgValue = 1
 $NewValue = $OrgValue
 $RetValueIfTrue = 4
