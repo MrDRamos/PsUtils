d2260a3347351208fdc188f25ea73815493f4fe5
MrDRamos <MrDRamos@Hotmail.com>
2022-03-26 01:33:13 -0400

    Wip - Test-WatchdogTimer.ps1

--- Threading/Test-WatchdogTimer.ps1
+++ Threading/Test-WatchdogTimer.ps1
@@ -21,52 +21,64 @@ https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/
 https://docs.microsoft.com/en-us/dotnet/api/system.timers.timer?view=net-6.0
 #>
 
-[int] $EventCount = 0 #Script scope variable
-$Script:G_Str = "hello world"
-[int] $G_Int = 22 
-$WatchDogTimerEventId = 'WatchDogTimer.Elapsed'
-
-# This demo has several mechanisms for the WatchDog event handler to signal back to the main loop when its done
-$WatchDogDoneEventId = $WatchDogDoneSignal = $WatchDogDoneFlag = 0
-# Uncomment one of the variable initializes below to activate that mechanism:
-$WatchDogDoneEventId = "Custom.Done-Event"
-#$WatchDogDoneSignal  = New-Object System.Threading.AutoResetEvent -ArgumentList $false
-#$WatchDogDoneFlag    = 1
-
 Function Remove-AppResources
 {
-    Write-Host "`nResetting Script Resources"
+    [CmdletBinding()]
+    param ()
 
-    if ($WatchDogTimer)
-    {
-        Write-Host "Removing WatchDogTimer"
-        $WatchDogTimer.Dispose()
-    }
+    Write-Verbose "`nReleasing Script Resources"
 
     if ($WatchDogDoneSignal)
     {
-        Write-Host "Removing Done-AutoResetEvent"
+        Write-Verbose "Removing Done-AutoResetEvent"
         $WatchDogDoneSignal.Dispose()
+        Remove-Variable -Name WatchDogDoneSignal -Scope Global
     }
 
-    $OldSubscription = Get-EventSubscriber -SourceIdentifier $WatchDogTimerEventId -ErrorAction Ignore
-    if ($OldSubscription)
+    if ($WatchDogTimerEventId)
     {
-        Write-Host "UnRegistering Event Subscribers"
-        ($OldSubscription | Format-List | Out-String).Trim() | Write-Verbose
-        Unregister-Event -SourceIdentifier $WatchDogTimerEventId
+        $OldSubscription = Get-EventSubscriber -SourceIdentifier $WatchDogTimerEventId -ErrorAction Ignore
+        if ($OldSubscription)
+        {
+            Write-Verbose "UnRegistering Event Subscribers"
+            ($OldSubscription | Format-List | Out-String).Trim() | Write-Verbose
+            Unregister-Event -SourceIdentifier $WatchDogTimerEventId
+            Remove-Variable -Name WatchDogTimerEventId -Scope Global
+        }    
     }
 
     # Remove Events in the Event Queue
     $OldEventS = Get-Event -ErrorAction Ignore
     if ($OldEventS)
     {
-        Write-Host "Removing Events in Queue:" -NoNewline
+        Write-Verbose "Removing Events in Queue:" -NoNewline
         ($OldEventS | Format-Table | Out-String).Trim() | Write-Verbose
         $OldEvents | Remove-Event #-SourceIdentifier $WatchDogTimerEventId
     }
+
+    if ($WatchDogTimer)
+    {
+        Write-Verbose "Removing WatchDogTimer"
+        $WatchDogTimer.Dispose()
+        #Remove-Variable -Name WatchDogTimer -Scope Script # Can't be removed
+        $WatchDogTimer = $null
+    }
 }
-Remove-AppResources
+Remove-AppResources # Just in case last test failed before final cleanup
+
+
+
+#Global scope is required to make variables visible in event handler
+$Global:EventCount = 0
+$Global:WatchDogTimerEventId = 'WatchDogTimer.Elapsed'
+
+# This demo has several mechanisms for the WatchDog event handler to signal back to the main loop when its done
+$Global:WatchDogDoneEventId = $Global:WatchDogDoneSignal = $Global:WatchDogDoneFlag = 0
+# Uncomment one of the variable initializes below to activate that mechanism:
+#$Global:WatchDogDoneEventId = "Custom.Done-Event"
+#$Global:WatchDogDoneSignal  = New-Object System.Threading.AutoResetEvent -ArgumentList $false
+$Global:WatchDogDoneFlag    = 1
+
 
 $WatchDogTimer = New-Object System.Timers.Timer -Property @{
     Interval  = $WatchDogSec * 1000
@@ -92,39 +104,44 @@ $WatchDogTimer = New-Object System.Timers.Timer -Property @{
     }
     #>
 
+    try 
+    {
     # No thread-save locking is needed to modify $EventCount, since the timer events run on the main thread
-    Write-Host "xxxx EventCount=$GEventCountStr" -ForegroundColor Green
-    $EventCount++   # The ++ seems to make a local copy
-    Write-Host "`nHandling elapsed event: $EventCount" -ForegroundColor Green
+    # But we need to access the variables of the main script block using the Global scope
+    $Global:EventCount++
+    Write-Host "`nAction handler received event: $Global:EventCount - $Global:WatchDogTimerEventId" -ForegroundColor Green
 
-    $RunSpace = Get-Runspace
-    $RuntimeInfo = @{
-        ThreadId         = [System.Threading.Thread]::CurrentThread.ManagedThreadId
-        HostRunspace     = $Host.Runspace.Id
-        RunspaceId       = $RunSpace.Id
+    $Verbose = $false
+    #$Verbose = $True
+    if ($Verbose)
+    {
+        $RunSpace = Get-Runspace
+        $RuntimeInfo = @{
+            ThreadId         = [System.Threading.Thread]::CurrentThread.ManagedThreadId
+            HostRunspace     = $Host.Runspace.Id
+            RunspaceId       = $RunSpace.Id
+        }
+        ($RuntimeInfo | Format-Table | Out-String).Trim() | Write-Host
+        
+        Write-Host "`$EventArgs:" -ForegroundColor Cyan
+        $EventArgs | ConvertTo-Json | Write-Host
+    
+        Write-Host "`$Event:" -ForegroundColor Cyan
+        $Event | ConvertTo-Json | Write-Host
+    
+        Write-Host "ThreadId: " -ForegroundColor Cyan -NoNewline
+        Write-Host ([System.Threading.Thread]::CurrentThread.ManagedThreadId)    
     }
-    ($RuntimeInfo | Format-Table | Out-String).Trim() | Write-Host
 
-    Write-Host "`$EventArgs:" -ForegroundColor Cyan
-    $EventArgs | ConvertTo-Json | Write-Host
-
-    Write-Host "`$Event:" -ForegroundColor Cyan
-    $Event | ConvertTo-Json | Write-Host
-
-    Write-Host "ThreadId: " -ForegroundColor Cyan -NoNewline
-    Write-Host ([System.Threading.Thread]::CurrentThread.ManagedThreadId)
-
-    Write-Host "xxxx Host=$($Host.Name) ID=$WatchDogDoneEventId Flag=$WatchDogDoneFlag" -ForegroundColor Green
-    Write-Host "xxxx G_Int=$G_Int" -ForegroundColor Green
-    Write-Host "xxxx G_Str=$G_Str" -ForegroundColor Green
+    #Region Notify Main loop
     # Notify Main loop that the Event handler is done using one of the mechenisms below:
-    $DoneMsg = "Finalizing elapsed event: $EventCount"
-    if ($WatchDogDoneSignal)
+    $DoneMsg = "Action handler exiting event: $Global:EventCount"
+    if ($Global:WatchDogDoneSignal)
     {
         Write-Host "$DoneMsg -> Setting Done-AutoResetEvent" -ForegroundColor Green
-        $WatchDogDoneSignal.Set()
+        $Global:WatchDogDoneSignal.Set()
     }
-    elseif ($WatchDogDoneEventId)
+    elseif ($Global:WatchDogDoneEventId)
     {
         if ($Host.Name -eq "ConsoleHost")
         {
@@ -138,27 +155,28 @@ $WatchDogTimer = New-Object System.Timers.Timer -Property @{
         else 
         {
             Write-Host "$DoneMsg -> Posting Custom-Done-Event" -ForegroundColor Green
-            $MsgData = @{EventCount = $EventCount; TimeGenerated = $Event.TimeGenerated }
-            New-Event -SourceIdentifier $WatchDogDoneEventId -Sender "WatchdogTimer.EventHandler" -MessageData $MsgData #-EventArguments
+            $MsgData = @{EventCount = $Global:EventCount; TimeGenerated = $Event.TimeGenerated }
+            New-Event -SourceIdentifier $Global:WatchDogDoneEventId -Sender "WatchdogTimer.EventHandler" -MessageData $MsgData #-EventArguments
         }
     }
-    elseif ($WatchDogDoneFlag)
+    elseif ($Global:WatchDogDoneFlag)
     {
         Write-Host "$DoneMsg -> Setting Done-Flag" -ForegroundColor Green
-        $null = [Threading.Interlocked]::CompareExchange([ref]$WatchDogDoneFlag, 2, 1)
+        $null = [Threading.Interlocked]::CompareExchange([ref]$Global:WatchDogDoneFlag, 2, 1)
     }
     else 
     {
         Write-Host $DoneMsg -ForegroundColor Green
     }
+    #EndRegion Notify Main loop
 
-    # Kill the application
-    if ($EventCount -ge 3)
+    #Region Kill the application
+    if ($Global:EventCount -ge 3)
     {
         <#
             # Terminate powershell Session
             # Method 1
-            $R = Get-Runspace
+            $R = [runspace]::DefaultRunspace
             $R[0].CloseAsync()
 
             # Method 2
@@ -171,10 +189,15 @@ $WatchDogTimer = New-Object System.Timers.Timer -Property @{
             [Environment]::Exit(1)
         #>
     }
-    
-    # The exception is silently caught by the Action invoker
-    #Throw "My Timeout Exception"
+    #EndRegion Kill the application
+    }
+    catch 
+    {    
+        # Note: Exception are silently caught by the event dispatcher
+        Write-Host "`nFatal error in event handler: $($_.Exception.Message)" -ForegroundColor Red
+    }
 }
+
 
 
 $RegisterParamS = @{
@@ -188,16 +211,19 @@ Write-Host "Registered WatchDogTimer Event Handler:"
 $TimerJob | Format-Table
 #EndRegion PsEvent
 
-
+#Region Main
 ##################### Main #####################
 Write-Host "Start Watch-Dog Timer Demo Script" -ForegroundColor Yellow
-$RunSpace = Get-Runspace
-$RuntimeInfo = @{
-    ThreadId         = [System.Threading.Thread]::CurrentThread.ManagedThreadId
-    HostRunspace     = $Host.Runspace.Id
-    RunspaceId       = $RunSpace.Id
+if ($VerbosePreference)
+{
+    $RunSpace = Get-Runspace
+    $RuntimeInfo = @{
+        ThreadId         = [System.Threading.Thread]::CurrentThread.ManagedThreadId
+        HostRunspace     = $Host.Runspace.Id
+        RunspaceId       = $RunSpace.Id
+    } 
+    ($RuntimeInfo | Format-Table | Out-String).Trim() | Write-Host
 }
-($RuntimeInfo | Format-Table | Out-String).Trim() | Write-Host
 
 Write-Host "Main loop waiting for WatchDog events:" -NoNewline
 $EndTime = (Get-Date).AddSeconds($TotalSec)
@@ -207,7 +233,6 @@ try
     do 
     {
         Write-Host "." -NoNewline
-
         if ($WatchDogDoneSignal)
         {
             if ($WatchDogDoneSignal.WaitOne($IntervalMiliSec))
@@ -242,10 +267,12 @@ try
 }
 catch
 {
-    Write-Host $_.Exception.Message
+    Write-Host "`n$_.Exception.Message" -ForegroundColor Red
 }
 
-# Free Event Resources
-Remove-AppResources
+$WatchDogTimer.Enabled = $false
+Write-Host "`nMain loop completed. Total Watch-Dog events: $EventCount"
+#EndRegion Main
 
-Write-Host "`nExiting Watch-Dog Timer Demo Script"
+# Free Event Resources
+Remove-AppResources #-Verbose
