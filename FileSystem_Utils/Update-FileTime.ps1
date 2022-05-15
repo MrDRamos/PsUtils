727e63895e0f8f22b49d8504869e8cfb281325ad
David Ramos <MrDRamos@Hotmail.com>
2022-05-15 06:37:15 -0400

    Added -verbose and -whatif switches to Update-FileTime.ps1

--- FileSystem_Utils/Update-FileTime.ps1
+++ FileSystem_Utils/Update-FileTime.ps1
@@ -1,6 +1,19 @@
+[CmdletBinding(SupportsShouldProcess = $true)]
+param (
+    [Parameter(Mandatory)]
+    [ValidateNotNull()]
+    [object] $Path, 
+
+    [Parameter()]
+    [datetime] $LastWriteTime = (Get-Date),
+
+    [Parameter()]
+    [switch] $Force
+)
+
 function Update-FileTime
 {
-    [CmdletBinding()]
+    [CmdletBinding(SupportsShouldProcess = $true)]
     param (
         [Parameter(Mandatory)]
         [ValidateNotNull()]
@@ -14,34 +27,41 @@ function Update-FileTime
     )
 
     # Validate/prepare the the folder
+    $Path = [System.IO.Path]::GetFullPath($Path)
     $Folder = Split-Path -Path $Path -Parent
     if (!(Resolve-Path -Path $Folder -ErrorAction Ignore))
     {
         if ($Force)
         {
-            $null = New-Item -ItemType Directory -Path $Folder
+            $null = New-Item -ItemType Directory -Path $Folder -Verbose:$VerbosePreference
         }
         else 
         {
             Write-Error "The folder does not exist: $Folder"
-            exit 1
+            return
         }
     }
 
     # Update the file
     if (!(Test-Path $Path)) 
     {
-        New-Item -ItemType File -Path $Path
+        $null = New-Item -ItemType File -Path $Path -Verbose:$VerbosePreference
     }
     else 
     {
-        (Get-ChildItem $Path).LastWriteTime = $LastWriteTime
+        $Operation = "Update LastWriteTime to: $LastWriteTime"
+        if ($PSCmdlet.ShouldProcess($Path, $Operation))
+        {
+            Write-Verbose "$Operation on file: `"$Path`"" -Verbose:$VerbosePreference
+            (Get-ChildItem $Path).LastWriteTime = $LastWriteTime
+        }
     }
 }
 
+Update-FileTime -Path $Path -LastWriteTime $LastWriteTime -Force:$Force
 
 # Define the alias: Touch
 if (!(Test-Path -Path Alias:Touch)) 
 {
-    New-Alias -Name Touch Update-FileTime -Force
+    New-Alias -Name 'Touch' -Value 'Update-FileTime'
 }
