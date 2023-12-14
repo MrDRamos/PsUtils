1f0cfa32f3ba809dfb147acdd03b551ea6fdb8e9
David Ramos <MrDRamos@Hotmail.com>
2023-12-14 03:24:38 -0500

    wip PsUpdate/Show-PsLatestVersion.ps1

--- PsUpdate/Show-PsLatestVersion.ps1
+++ PsUpdate/Show-PsLatestVersion.ps1
@@ -4,12 +4,14 @@ Write-Host "GitHub Links:" -ForegroundColor Cyan
     ChangeLog = "https://github.com/PowerShell/PowerShell/tree/master/CHANGELOG"
     Releases  = "https://github.com/PowerShell/PowerShell/releases"
     Install   = 'https://learn.microsoft.com/en-us/powershell/scripting/install/installing-powershell-on-windows'
-    MsiPkgUrl = "`"https://github.com/PowerShell/PowerShell/releases/download/v`$VER/PowerShell-`$VER-win-x64.msi`""
 } | Format-List | Out-String).Trim()
 
 Write-Host "`nRelease Versions:" -ForegroundColor Cyan
 $metadata = Invoke-RestMethod "https://raw.githubusercontent.com/PowerShell/PowerShell/master/tools/metadata.json"
 ($metadata | Format-List StableReleaseTag, PreviewReleaseTag, LTSReleaseTag | Out-String).Trim()
+$release = $metadata.StableReleaseTag -replace '^v'
+$packageName = "PowerShell-${release}-win-x64.msi"
+$downloadURL = "https://github.com/PowerShell/PowerShell/releases/download/v${release}/${packageName}"
 
 <#
 Write-Host "`nDaily Builds:" -ForegroundColor Cyan
@@ -33,6 +35,7 @@ Write-Host "`nInstall stable release using:" -ForegroundColor Cyan
   Invoke-Expression "& { `$(Invoke-RestMethod 'https://aka.ms/install-powershell.ps1') } -UseMSI"
 
 #2) Download Stable MSI Release: $($metadata.StableReleaseTag)
+  IRM $downloadURL -Outfile $packageName
 
 #3) Install using winget:
   winget install --id Microsoft.Powershell --source winget
@@ -44,14 +47,7 @@ Write-Host "`nInstall stable release using:" -ForegroundColor Cyan
 switch (Read-Host -Prompt "`n? Install now using method #1..4 ") 
 { 
     1 { Invoke-Expression "& { $(Invoke-RestMethod 'https://aka.ms/install-powershell.ps1') } -UseMSI" }
-    2 {
-        $metadata = Invoke-RestMethod "https://raw.githubusercontent.com/PowerShell/PowerShell/master/tools/metadata.json"
-        $release = $metadata.StableReleaseTag -replace '^v'
-        $packageName = "PowerShell-${release}-win-x64.msi"
-        $downloadURL = "https://github.com/PowerShell/PowerShell/releases/download/v${release}/${packageName}"
-        Write-Host "Downloading from: $downloadURL"
-        Invoke-WebRequest -Uri $downloadURL -OutFile $packageName
-      }
+    2 { Invoke-WebRequest -Uri $downloadURL -OutFile $packageName }
     3 { & winget install --id Microsoft.Powershell --source winget }
     4 {
         Invoke-RestMethod -Method Get -Uri "https://aka.ms/Install-Powershell.ps1" -OutFile .\Install-Powershell.ps1
